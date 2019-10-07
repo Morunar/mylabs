e3ac8cdb90f988e5ac5cdd3a1099f485ca1e363c
Vladyslav <vladyslav05072001v@gmail.com>
2019-10-07 11:43:27 +0300

    Lab6

---
+++ Labs6/Labs6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs6
+{
+    class Program
+    {
+        static void Main()
+        {
+            object N;
+            int J;
+            List<object> types = new List<object>() {1,2,4,"lal","hello",true,1.2,'A'} ;
+
+
+            do
+            {
+                Console.WriteLine("Enter J(the number of items you want to enter)");
+                J = Int32.Parse(Console.ReadLine());
+                if (J <= 0)
+                {
+                    Console.WriteLine("Error;");
+                }
+
+            } while (J <= 0);
+           
+            for (int i = 0;i < J;i++){
+               N = (Console.ReadLine());
+               types.Add(N);
+            }
+
+            
+            for (int i = 0; i < types.Count; i++)
+            {
+                Console.WriteLine("Тип элемента {0} = {1}",types[i], types[i].GetType());
+                
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
