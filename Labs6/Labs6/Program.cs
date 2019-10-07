c7e9b31fcc8ce0b8fe7c3c357409e9c743898e3c
Vladyslav <vladyslav05072001v@gmail.com>
2019-10-07 15:37:07 +0300

    Update Program.cs

--- Labs6/Labs6/Program.cs
+++ Labs6/Labs6/Program.cs
@@ -11,7 +11,7 @@ namespace Labs6
         static void Main()
         {
             object N;
-            int J;
+            int J,K;
             List<object> types = new List<object>() {1,2,4,"lal","hello",true,1.2,'A'} ;
 
 
@@ -25,10 +25,53 @@ namespace Labs6
                 }
 
             } while (J <= 0);
-           
+            Console.WriteLine("K its data type.");
+            Console.WriteLine("K = 0,this type Bool.");
+            Console.WriteLine("K = 1,this type Char.");
+            Console.WriteLine("K = 2,this type String.");
+            Console.WriteLine("K = 3,this type Int.");
+            Console.WriteLine("K = 4,this type Double.");
+
             for (int i = 0;i < J;i++){
-               N = (Console.ReadLine());
-               types.Add(N);
+                do
+                {
+                    Console.WriteLine("Enter K:");
+                    K = Int32.Parse(Console.ReadLine());
+                    if (K < 0 || K > 4)
+                    {
+                        Console.WriteLine("Error;");
+                    }
+                } while (K < 0 || K > 4);
+                switch (K)
+                {
+                    case 0:
+                        Console.WriteLine("Enter element:");
+                        N = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                        types.Add(N);
+                        break;
+                    case 1:
+                        Console.WriteLine("Enter element:");
+                        N = Char.Parse(Console.ReadLine());
+                        types.Add(N);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter element:");
+                        N = (Console.ReadLine());
+                        types.Add(N);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter element:");
+                        N = Int32.Parse(Console.ReadLine());
+                        types.Add(N);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter element:");
+                        N = Double.Parse(Console.ReadLine());
+                        types.Add(N);
+                        break;
+
+                }
+               
             }
 
             
@@ -37,9 +80,41 @@ namespace Labs6
                 Console.WriteLine("Тип элемента {0} = {1}",types[i], types[i].GetType());
                 
             }
+            int SumInt, SumChar, SumBool, SumString, SumDouble;
+            SumInt = 0;
+            SumChar = 0;
+            SumBool = 0;
+            SumString = 0;
+            SumDouble = 0;
+            for (int i = 0; i < types.Count; i++)
+            {
+              if (types[i] is string) 
+                {
+                    SumString++;
+                }
+              else if (types[i] is int)
+                {
+                    SumInt++;
+                }
+              else if (types[i] is bool)
+                {
+                    SumBool++;
+                }
+              else if (types[i] is char)
+                {
+                    SumChar++;
+                }
+              else if (types[i] is double)
+                {
+                    SumDouble++;
+                }
+           }
 
-
-
+            Console.WriteLine("Количество элементов типа String = {0}",SumString);
+            Console.WriteLine("Количество элементов типа Int = {0}", SumInt);
+            Console.WriteLine("Количество элементов типа Char = {0}", SumChar);
+            Console.WriteLine("Количество элементов типа Bool = {0}", SumBool);
+            Console.WriteLine("Количество элементов типа Double = {0}", SumDouble);
 
             Console.ReadKey();
 
