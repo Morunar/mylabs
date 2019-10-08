99b4ab2b43e14a286258e4370a5d780bc220e461
Vladyslav <vladyslav05072001v@gmail.com>
2019-10-08 17:39:18 +0300

    amendments for lab2

--- lab2/ConsoleApp3/Program.cs
+++ lab2/ConsoleApp3/Program.cs
@@ -6,12 +6,12 @@ namespace Lab_2
     {
         static void Main()
         {
-            double k, n,S,N;
+            double k, n, S, N;
             Console.WriteLine("Enter n:");
             do
             {
                 n = Single.Parse(Console.ReadLine());
-                if(n < 0)
+                if (n < 0)
                 {
                     Console.WriteLine("Error.");
                 }
@@ -19,12 +19,19 @@ namespace Lab_2
             k = n * n;
             N = n * k;
             S = 0;
-            for(; k <= N; k++)
+            if (0 <= n * n || n * n <= n * k)
             {
-                S = S + ((Math.Pow(k, 3) - 3)/(Math.Pow(-1,k)*Math.Pow(k,2)+5));
-                
+                for (; k <= N; k++)
+                {
+                    S = S + ((Math.Pow(k, 3) - 3) / (Math.Pow(-1, k) * Math.Pow(k, 2) + 5));
+
+                }
+                Console.WriteLine("S ={0} ", S);
             }
-            Console.WriteLine("S ={0} ",S);
+            else
+            {
+                Console.WriteLine("Error;");
+            }
             Console.ReadKey();
 
         }
