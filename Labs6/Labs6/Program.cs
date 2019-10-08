3a6ad0356b14c5b56355ed278a4c300e1ad7b426
Vladyslav <vladyslav05072001v@gmail.com>
2019-10-08 17:47:29 +0300

    amendments for labs 6

--- Labs6/Labs6/Program.cs
+++ Labs6/Labs6/Program.cs
@@ -11,68 +11,62 @@ namespace Labs6
         static void Main()
         {
             object N;
-            int J,K;
+            int K;
             List<object> types = new List<object>() {1,2,4,"lal","hello",true,1.2,'A'} ;
 
 
-            do
-            {
-                Console.WriteLine("Enter J(the number of items you want to enter)");
-                J = Int32.Parse(Console.ReadLine());
-                if (J <= 0)
-                {
-                    Console.WriteLine("Error;");
-                }
-
-            } while (J <= 0);
+         
             Console.WriteLine("K its data type.");
             Console.WriteLine("K = 0,this type Bool.");
             Console.WriteLine("K = 1,this type Char.");
             Console.WriteLine("K = 2,this type String.");
             Console.WriteLine("K = 3,this type Int.");
             Console.WriteLine("K = 4,this type Double.");
-
-            for (int i = 0;i < J;i++){
+            Console.WriteLine("If you want to stop program.Write 5.");
+            do
+            {
                 do
                 {
                     Console.WriteLine("Enter K:");
                     K = Int32.Parse(Console.ReadLine());
-                    if (K < 0 || K > 4)
+                    if (K < 0 || K > 5)
                     {
                         Console.WriteLine("Error;");
                     }
-                } while (K < 0 || K > 4);
-                switch (K)
+                } while (K < 0 || K > 5);
+                if (K >= 0 || K <= 4)
                 {
-                    case 0:
-                        Console.WriteLine("Enter element:");
-                        N = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        types.Add(N);
-                        break;
-                    case 1:
-                        Console.WriteLine("Enter element:");
-                        N = Char.Parse(Console.ReadLine());
-                        types.Add(N);
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter element:");
-                        N = (Console.ReadLine());
-                        types.Add(N);
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter element:");
-                        N = Int32.Parse(Console.ReadLine());
-                        types.Add(N);
-                        break;
-                    case 4:
-                        Console.WriteLine("Enter element:");
-                        N = Double.Parse(Console.ReadLine());
-                        types.Add(N);
-                        break;
+                    switch (K)
+                    {
+                        case 0:
+                            Console.WriteLine("Enter element(numbers):");
+                            N = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                            types.Add(N);
+                            break;
+                        case 1:
+                            Console.WriteLine("Enter element:");
+                            N = Char.Parse(Console.ReadLine());
+                            types.Add(N);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter element:");
+                            N = (Console.ReadLine());
+                            types.Add(N);
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter element:");
+                            N = Int32.Parse(Console.ReadLine());
+                            types.Add(N);
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter element:");
+                            N = Double.Parse(Console.ReadLine());
+                            types.Add(N);
+                            break;
 
+                    }
                 }
-               
-            }
+            } while (K != 5);
 
             
             for (int i = 0; i < types.Count; i++)
